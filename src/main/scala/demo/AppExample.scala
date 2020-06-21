c98850ab53f445188dbb44f2ca5a16fae5dedd3f
sk-sushil <40827825+sk-sushil@users.noreply.github.com>
2020-06-21 20:22:16 +0530

    Delete AppExample.scala

--- src/main/scala/demo/AppExample.scala
+++
@@ -1,8 +0,0 @@
-package demo
-
-//App is a helper class that holds the main method and its Members together.
-// The App trait can be used to quickly turn Objects into Executable programs.
-object AppExample extends App {
-  println("App example...")
-
-}
